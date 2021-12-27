ba57211b9c7dd23a77cac609c7bd8135c54211be
flysafely <616604060@qq.com>
2021-12-27 19:26:04 +0800

    2021.12.27-动态监听测试

---
+++ DMUTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace DMUTest
+{
+    [Transaction(TransactionMode.Manual)]
+    public class DMUTest : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication app = commandData.Application;
+            AddInId addInId = app.ActiveAddInId;
+            Guid updaterGuid = new Guid("BC0CC3B1-03B8-4072-B167-4B69C1FBEAED");
+            ParameterUpdater parameterUpdater = new ParameterUpdater(addInId, updaterGuid);
+            // 注册updater
+            UIDocument activeUIDoc = commandData.Application.ActiveUIDocument;
+            Document activeDoc = activeUIDoc.Document;
+            UpdaterRegistry.RegisterUpdater(parameterUpdater, true);
+            Element elementEle = activeDoc.GetElement(new ElementId(529759));
+            var parameter = elementEle.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED);
+            UpdaterRegistry.AddTrigger(parameterUpdater.GetUpdaterId(), activeDoc, new List<ElementId> { new ElementId(529759) }, Element.GetChangeTypeAny());
+            TaskDialog.Show("note", "123");
+            using (Transaction trans = new Transaction(activeDoc, "aaa"))
+            {   
+                trans.Start();
+                activeDoc.Delete(new ElementId(529759));
+                trans.Commit();
+            }
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ ParameterUpdater.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace DMUTest
+{
+    internal class ParameterUpdater : IUpdater
+    {
+        UpdaterId _uid;
+        public ParameterUpdater(AddInId addinID, Guid updaterGuid)
+        {
+
+            _uid = new UpdaterId(addinID, updaterGuid);
+        }
+        public void Execute(UpdaterData data)
+        {
+            Func<ICollection<ElementId>, string> toString = ids => ids.Aggregate("", (ss, id) => ss + "," + id).TrimStart(',');
+            var sb = new StringBuilder();
+            sb.AppendLine("added:" + toString(data.GetAddedElementIds()));
+            sb.AppendLine("modified:" + toString(data.GetModifiedElementIds()));
+            sb.AppendLine("deleted:" + toString(data.GetDeletedElementIds()));
+            TaskDialog.Show("Changes", sb.ToString());
+        }
+
+        public string GetAdditionalInformation()
+        {
+            return "N/A";
+        }
+
+        public ChangePriority GetChangePriority()
+        {
+            return ChangePriority.FreeStandingComponents;
+        }
+
+        public UpdaterId GetUpdaterId()
+        {
+            return _uid;
+        }
+
+        public string GetUpdaterName()
+        {
+            return "ParameterUpdater";
+        }
+    }
+}
